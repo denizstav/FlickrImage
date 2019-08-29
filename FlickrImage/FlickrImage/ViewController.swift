19b8632a0f0dd84476ad446838f15dc946e4535a
Deniz Tav <dtav@mobiquityinc.com>
2019-08-29 20:37:21 +0200

    + initial commit - creating single view app

---
+++ FlickrImage/FlickrImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrImage
+//
+//  Created by Deniz Tav on 29/08/2019.
+//  Copyright © 2019 Deniz Tav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
